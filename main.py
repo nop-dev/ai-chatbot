d70f41a7cc6dfb3d9422285ecb491b66ffff73f5
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-05-10 07:53:00 -0300

    feat: :sparkles: adiciona dinâmica de chat

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import google.generativeai as genai
 GOOGLE_API_KEY= 'AIzaSyD3i22rqClUTsqZbIUFG0OBZnRXfSq-5zk'
 genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-pro')
-
 generation_config = {
-    "candidates_count": 1,
+    "candidate_count": 1,
     "temperature": 0.5
 }
 
@@ -17,7 +15,13 @@ safety_settings = {
     "DANGEROUS": "BLOCK_NONE"
 }
 
-model - genai.GenerativeModel(model_name="gemini-1.0-pro-", generation_config=generation_config, safety_settings=safety_settings)
+model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config, safety_settings=safety_settings)
 
-response = model.generate_content("Me diga 10 cores diferente")
-print(response.text)
+chat = model.start_chat(history=[])
+
+prompt = input("Escreva seu comando: ")
+
+while prompt != "exit":
+    response = chat.send_message(prompt)
+    print("Resposta: ", response.text, "\n")
+    prompt = input("Escreva seu comando: ")
