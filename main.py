f296ca670a84ea3427f2f086f012ba9c5c7f907e
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-05-11 11:25:33 -0300

    feat: :sparkles: últimos ajustes feitos, chatbot concluído...

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import google.generativeai as genai
+import textwrap
+from IPython.display import display, Markdown
 
 GOOGLE_API_KEY= 'AIzaSyD3i22rqClUTsqZbIUFG0OBZnRXfSq-5zk'
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -19,9 +21,18 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=gen
 
 chat = model.start_chat(history=[])
 
-prompt = input("Escreva seu comando: ")
+prompt = input('Esperando prompt: ')
 
 while prompt != "exit":
-    response = chat.send_message(prompt)
-    print("Resposta: ", response.text, "\n")
-    prompt = input("Escreva seu comando: ")
+  response = chat.send_message(prompt)
+  print("Resposta:", response.text, '\n\n')
+
+  prompt = input('Escreva seu comando: ')
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+# for message in chat.history: (disponível apenas para ambiente Jupyter Notebook)
+  display(to_markdown(f'**{message.role}**: {message.parts[0].text}'))
+  print('-------------------------------------------')
